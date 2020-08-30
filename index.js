f6b6508cfed262c666aa57bd5a43010c1c2f0b79
oscdiazl <os.diazlobos@gmail.com>
2020-08-30 19:15:40 -0400

    corregidos i.js

--- index.js
+++ index.js
@@ -82,6 +82,12 @@ app.use((error, req,res, next) => {
 }); 
 
 
-app.listen(process.env.PUERTO, () => {
-    console.log('servidor corriendo'); 
-});
+
+//servidor y puerto
+const host = process.env.HOST || '0.0.0.0'; 
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, () => {
+    console.log('El servidor esta listo');
+})
+
